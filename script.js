f248581f3e7be359f32729c00986d77619050a85
Abdelrahman-Mamdouh16 <bedo.lela32@gmail.com>
2023-07-31 20:25:31 +0300

    update

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ var productPriceData = document.getElementById("productPrice");
 var productCategoryData = document.getElementById("productCategory");
 var productDescriptionData = document.getElementById("productDescription");
 var SearchInput = document.getElementById("SearchInput");
-
 var addBtn = document.getElementById("addBtn");
 var updateBtn = document.getElementById("updateBtn");
-
+var alert = document.getElementById("alert");
+var productNameAlert = document.querySelector("#productNameAlert");
+var productPriceAlert = document.querySelector("#productPriceAlert");
+var productCategoryAlert = document.querySelector("#productCategoryAlert");
+var productDescriptionAlert = document.querySelector("#productDescriptionAlert");
 
 var totalIndex = 0;
 var productCounter = [];
@@ -19,16 +22,27 @@ if (localStorage.getItem("products") != null) {
 
 function addProduct() {
 
-    var product = {
-        name: productNameData.value,
-        price: productPriceData.value,
-        category: productCategoryData.value,
-        description: productDescriptionData.value,
+    if (productNameValid() == false || productPriceValid() == false || productCategoryValid() == false || productDescriptionValid() == false) {
+        alert.innerHTML =`<div class="alert alert-danger" role="alert">
+        <span style="background-color: transparent;" class="d-block">Product Name must contain at least 3 characters </span>
+        <span style="background-color: transparent;" class="d-block">Product Price must be a valid Number</span>
+        <span style="background-color: transparent;" class="d-block">Product Category must contain at least 3 characters</span>
+        <span style="background-color: transparent;" class="d-block">Product Description must be a valid one</span>
+        </div>`;
+        clearValidation();
+    } else {
+        var product = {
+            name: productNameData.value,
+            price: productPriceData.value,
+            category: productCategoryData.value,
+            description: productDescriptionData.value,
+        }
+        productCounter.push(product);
+        localStorage.setItem("products", JSON.stringify(productCounter));
+        displayData();
+        clearData();
+        clearValidation();
     }
-    productCounter.push(product);
-    localStorage.setItem("products", JSON.stringify(productCounter));
-    displayData();
-    clearData();
 }
 
 
@@ -38,7 +52,7 @@ function displayData() {
     for (i = 0; i < productCounter.length; i++) {
         var productData = productCounter[i];
         container += `<tr>
-                            <td>${i}</td>
+                            <td>${i + 1}</td>
                             <td> ${productData.name} </td>
                             <td> ${productData.price} </td>
                             <td> ${productData.category} </td>
@@ -57,6 +71,29 @@ function clearData() {
     productPriceData.value = "";
     productCategoryData.value = "";
     productDescriptionData.value = "";
+}
+function clearValidation() {
+    productNameData.classList.remove("is-valid");
+    productNameData.classList.remove("is-invalid");
+    productNameAlert.classList.remove("d-block");
+    productNameAlert.classList.add("d-none");
+    //!----------------------------------------
+    productPriceData.classList.remove("is-valid");
+    productPriceData.classList.remove("is-invalid");
+    productPriceAlert.classList.remove("d-block");
+    productPriceAlert.classList.add("d-none");
+    //!----------------------------------------
+    productCategoryData.classList.remove("is-valid");
+    productCategoryData.classList.remove("is-invalid");
+    productCategoryAlert.classList.remove("d-block");
+    productCategoryAlert.classList.add("d-none");
+    //!----------------------------------------
+    productDescriptionData.classList.remove("is-valid");
+    productDescriptionData.classList.remove("is-invalid");
+    productDescriptionAlert.classList.remove("d-block");
+    productDescriptionAlert.classList.add("d-none");
+    //!----------------------------------------
+
 }
 
 
@@ -118,4 +155,91 @@ function updateProduct() {
     addBtn.classList.remove("d-none");
     updateBtn.classList.add("d-none");
     clearData()
+}
+
+
+function productNameValid() {
+    var productNameRegex = /^[A-Z][a-z]{1,}\s?([A-Z][a-z]{1,})?\s?([A-Z][a-z]{1,})?$/;
+    var Name = productNameData.value;
+    if (productNameRegex.test(Name) == true) {
+        productNameData.classList.add("is-valid");
+        productNameData.classList.remove("is-invalid");
+        productNameAlert.classList.remove("d-block");
+        productNameAlert.classList.add("d-none");
+        alert.innerHTML = "";
+        return true;
+    }
+    else {
+        productNameData.classList.add("is-invalid");
+        productNameData.classList.remove("is-valid");
+        productNameAlert.classList.add("d-block")
+        productNameAlert.classList.remove("d-none");
+        return false;
+
+    }
+}
+
+function productPriceValid() {
+    var productPriceRegex = /^[0-9]{3,10}$/;
+    var price = productPriceData.value;
+
+    console.log(price);
+
+    if (productPriceRegex.test(price) == true) {
+        productPriceData.classList.add("is-valid");
+        productPriceData.classList.remove("is-invalid");
+        productPriceAlert.classList.remove("d-block");
+        productPriceAlert.classList.add("d-none");
+        alert.innerHTML = "";
+        return true;
+    }
+    else {
+        productPriceData.classList.add("is-invalid");
+        productPriceData.classList.remove("is-valid");
+        productPriceAlert.classList.add("d-block")
+        productPriceAlert.classList.remove("d-none");
+        return false;
+
+    }
+}
+
+function productCategoryValid() {
+    var productCategoryRegex = /^[A-Z][a-z]{1,}\s?([a-z]{1,})?\s?([a-z]{1,})?\s?([a-z]{1,})?\s?([a-z]{1,})?\s?([a-z]{1,})?$/;
+    var Category = productCategoryData.value;
+    if (productCategoryRegex.test(Category) == true) {
+        productCategoryData.classList.add("is-valid");
+        productCategoryData.classList.remove("is-invalid");
+        productCategoryAlert.classList.remove("d-block");
+        productCategoryAlert.classList.add("d-none");
+        alert.innerHTML = "";
+        return true;
+    }
+    else {
+        productCategoryData.classList.add("is-invalid");
+        productCategoryData.classList.remove("is-valid");
+        productCategoryAlert.classList.add("d-block");
+        productCategoryAlert.classList.remove("d-none");
+        return false;
+
+    }
+}
+
+function productDescriptionValid() {
+    var productDescriptionRegex = /^[A-Z][a-z]{1,}\s?([a-z]{1,})?\s?([a-z]{1,})?\s?([a-z]{1,})?\s?([a-z]{1,})?\s?([a-z]{1,})?\s?([a-z]{1,})?\s?([a-z]{1,})?\s?([a-z]{1,})?\s?([a-z]{1,})?$/;
+    var Description = productDescriptionData.value;
+    if (productDescriptionRegex.test(Description) == true) {
+        productDescriptionData.classList.add("is-valid");
+        productDescriptionData.classList.remove("is-invalid");
+        productDescriptionAlert.classList.remove("d-block");
+        productDescriptionAlert.classList.add("d-none");
+        alert.innerHTML = "";
+        return true;
+    }
+    else {
+        productDescriptionData.classList.add("is-invalid");
+        productDescriptionData.classList.remove("is-valid");
+        productDescriptionAlert.classList.add("d-block");
+        productDescriptionAlert.classList.remove("d-none");
+        return false;
+    }
 }
